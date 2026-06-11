7b4665e23bf5b54abc5d2f6471aec940888886f3
100zowo <100zowo@gmail.com>
2026-06-11 13:47:25 +0900

    fix. wait modify

--- server/src/middleware/loginMiddleware.js
+++ server/src/middleware/loginMiddleware.js
@@ -13,7 +13,7 @@ const SERVICE_CONFIG = {
     loginFn:    (page) => BaeminService.login(page, process.env.BAEMIN_ID, process.env.BAEMIN_PW),
     checkLogin: async (page) => {
       try {
-        await goto(page, process.env.BAEMIN_URL, 10000, 'networkidle2');
+        await goto(page, process.env.BAEMIN_URL, 10000, 'domcontentloaded');
         return page.url().includes(process.env.BAEMIN_URL);
       } catch {
         return false;
